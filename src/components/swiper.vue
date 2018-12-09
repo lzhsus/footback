7141d9c366d2cb8a5e92a79b8a0e99ac9fdddeb9
lzh <1434253600@qq.com>
2018-12-09 16:34:28 +0800

    swiper

---
+++ src/components/swiper.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-carousel arrow="never" :interval="4000" type height="200px">
+    <el-carousel-item v-for="(item, index) in arrSwiper" :key="index">
+     <img :id="item.id" :name="item.name" :src="item.imgUrl" alt="" srcset="">
+    </el-carousel-item>
+  </el-carousel>
+</template>
+<script>
+export default {
+  name: "swiper",
+  data() {
+    return {
+
+    };
+  },
+  props: ["arrSwiper"]
+};
+</script>
+<style>
+.el-carousel__item img{
+  width: 100%;
+  height: 100%;
+}
+.el-carousel__button {
+  width: 14px;
+  background-color: #ccc;
+}
+.el-carousel__indicator.is-active button.el-carousel__button {
+  background-color: red;
+}
+</style>
+
