d8f630a6a2e5be6f7f57a34d59346a985feae710
lzh <1434253600@qq.com>
2018-12-10 23:33:40 +0800

    首页频道展示

--- src/components/navChannel/navChannel.vue
+++ src/components/navChannel/navChannel.vue
@@ -1,74 +1,169 @@
 <template>
   <div class="navChannel">
-      <div class="mychannel">
-        <div class="mytit">
-          <span>{{mychannelTit}}</span><span>{{mychannelTowTit}}</span>
-        </div>
-        <div class="myok">
-          <div v-for="(item, index) in mychannel" :key="index" id="item.id">
-            <span>{{item.tit}}</span>
-          </div>
+    <div class="chammelTop">
+      <div>
+        <router-link to="/home">
+          <i class="el-icon-arrow-left"></i>
+        </router-link>
+      </div>
+      <div>管理我的频道</div>
+      <div>
+        <router-link to="/home">完成</router-link>
+      </div>
+    </div>
+
+    <div class="mychannel">
+      <div class="mytit">
+        <span>{{mychannelTit}}</span>
+        <span>{{mychannelTowTit}}</span>
+      </div>
+      <div class="myok">
+        <div v-for="(item, index) in mychannel" :key="index" :id="item.id">
+          <span>{{item.tit}}</span>
+          <i :id="item.id" class="el-icon-close" @click="delChannel(item.id)"></i>
         </div>
       </div>
+    </div>
+
+    <div class="mychannel">
+      <div class="mytit">
+        <span>{{newchannelTit}}</span>
+        <span>{{newchannelTowTit}}</span>
+      </div>
+      <div class="myok">
+        <div v-for="(item, index) in newchannel" :key="index" :id="item.id">
+          <span @click="addChannel(item.id)">+{{item.tit}}</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import store from "../../store/store";
 export default {
   name: "navChannel",
   data() {
     return {
-      mychannel:[
-        {"tit":'热门',"id":'0'},
-         {"tit":'热门',"id":'2'},
-          {"tit":'热门',"id":'3'},
-           {"tit":'热门',"id":'4'},
-            {"tit":'热门',"id":'5'},
-             {"tit":'热门',"id":'6'},
-              {"tit":'热门',"id":'7'},
-               {"tit":'热门',"id":'8'},
-                {"tit":'热门',"id":'9'},
-      ],
-      mychannelTit:'我的频道',
-      mychannelTowTit:'拖拽可以排序',
-      newchannel:[]
+      mychannel: [],
+      mychannelTit: "我的频道",
+      mychannelTowTit: "拖拽可以排序",
+      newchannelTit: "频道推荐",
+      newchannelTowTit: "点击添加频道",
+      newchannel: []
     };
   },
   methods: {
+    //返回到首页
+    goHome: function() {
+      this.$route({ path: "/home" });
+    },
+    //删除选择的频道
+    delChannel(id) {
+      // 读取本地存储 newlist
+      var selectIDlistMy = store.fetchIDlist("newlist") || []; //读取
+      // 删除本地存储 mylist
+      store.removeIDlist("mylist");
+      var mynewArr = [];
+
+      for (var i = 0; i < this.mychannel.length; i++) {
+        var index = i;
+        if (this.mychannel[i].id != parseInt(id)) {
+          console.log(i);
+          mynewArr.push(this.mychannel[i]);
+        } else {
+          selectIDlistMy.push(this.mychannel[i]);
+        }
+      }
+      // 创建 保存本地存储 mylist
+      store.saveIDlist(mynewArr, "mylist"); //保存
+      this.mychannel = mynewArr;
+      // 获取 保存到本地存储 newlist
+      store.saveIDlist(selectIDlistMy, "newlist"); //保存
+      this.newchannel = selectIDlistMy;
+    },
+    addChannel(id) {
+      // 读取本地存储 newlist
+      var selectIDlistMy = store.fetchIDlist("mylist") || []; //读取
+      store.removeIDlist("newlist");
+      var newArr = [];
+      for (var i = 0; i < this.newchannel.length; i++) {
+        var index = i;
+        if (this.newchannel[i].id != parseInt(id)) {
+          newArr.push(this.newchannel[i]);
+        } else {
+          selectIDlistMy.push(this.newchannel[i]);
+        }
+      }
+      // 创建 保存本地存储 mylist
+      store.saveIDlist(selectIDlistMy, "mylist"); //保存
+      this.mychannel = selectIDlistMy;
+      // 获取 保存到本地存储 newlist
+      store.saveIDlist(newArr, "newlist"); //保存
+      this.newchannel = newArr;
+    }
+  },
+  mounted() {
+    var selectIDlistMy = store.fetchIDlist("mylist"); //读取
+    var selectIDlistNew = store.fetchIDlist("newlist"); //读取
+    this.mychannel = selectIDlistMy;
+    this.newchannel = selectIDlistNew;
   }
 };
 </script>
 <style>
-.mychannel>div{
+.chammelTop {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+  font-size: 20px;
+  background-color: rgb(244, 247, 247);
+}
+.chammelTop i {
+  font-size: 26px;
+}
+
+.mychannel > .mytit {
   width: 100%;
-  margin: 40px;
+  padding: 0px 20px 0;
   box-sizing: border-box;
 }
-.mychannel>.mytit span:nth-of-type(1){
-  font-size: 50px;
+.mychannel > .mytit span:nth-of-type(1) {
+  font-size: 20px;
   color: #000;
-  margin-right: 20px;
+  margin-right: 10px;
 }
-.mychannel>div:nth-of-type(1) span:nth-of-type(2){
-  font-size: 30px;
+.mychannel > .mytit span:nth-of-type(2) {
+  font-size: 15px;
   color: #ccc;
 }
-.mychannel>div:nth-of-type(2)::after{
+.mychannel > div:nth-of-type(2)::after {
   display: block;
   clear: both;
-  content: ""
+  content: "";
 }
-.mychannel>div:nth-of-type(2){
+.mychannel > .myok {
   box-sizing: border-box;
+  padding: 0 20px;
 }
-.mychannel>div:nth-of-type(2) div{
+.mychannel > .myok div {
   float: left;
-  margin: 30px;
+  margin: 0 4px;
+  position: relative;
 }
-.mychannel>div:nth-of-type(2) span:nth-of-type(1){
-  font-size: 40px;
+.mychannel > .myok span {
+  font-size: 18px;
   color: #000;
-  padding: 10px 20px;
   border: 1px solid #ccc;
+  padding: 6px 20px;
+}
+.mychannel > .myok i {
+  border: 1px solid rgb(212, 212, 212);
+  border-radius: 50%;
+  font-size: 10px;
+  position: absolute;
+  right: -6px;
+  top: 14px;
 }
 </style>
 
--- src/store/store.js
+++ src/store/store.js
@@ -2,3 +2,32 @@ import Vue from 'vue';
 // 这是状态管理器，用来管理Vue的所有组件状态
 import Vuex from 'vuex';
 Vue.use(Vuex);
+
+const MY_IDLIST_KEY = 'mylist'; //定义常量保存键值
+const New_IDLIST_KEY = 'newlist'; //定义常量保存键值
+
+export default {
+
+  saveIDlist(data, value) {
+    if (value == "mylist") {
+      window.localStorage.setItem(MY_IDLIST_KEY, JSON.stringify(data));
+    } else if (value == "newlist") {
+      window.localStorage.setItem(New_IDLIST_KEY, JSON.stringify(data));
+    }
+  },
+
+  fetchIDlist(value) {
+    if (value == "mylist") {
+      return JSON.parse(window.localStorage.getItem(MY_IDLIST_KEY) || '[]');
+    } else if (value == "newlist") {
+      return JSON.parse(window.localStorage.getItem(New_IDLIST_KEY) || '[]');
+    }
+  },
+  removeIDlist(value){
+    if (value == "mylist") {
+      window.localStorage.removeItem('MY_IDLIST_KEY')
+    } else if (value == "newlist") {
+      window.localStorage.removeItem('New_IDLIST_KEY')
+    }
+  }
+}
